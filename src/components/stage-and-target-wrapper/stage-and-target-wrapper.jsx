812d74439021edaff97e1641849192cbbec1f285
zl <zl@qq.com>
2023-02-16 09:33:16 +0800

    fix: 解决移动端舞台消失和预览窗口无法操作

--- src/components/stage-and-target-wrapper/stage-and-target-wrapper.jsx
+++ src/components/stage-and-target-wrapper/stage-and-target-wrapper.jsx
@@ -24,7 +24,7 @@ const StageAndTargetWrapper = props => {
         ...componentProps
     } = omit(props, 'dispatch');
     return (
-        <Box className={classNames(styles.stageAndTargetWrapper, styles[stageSize], isPreview  && stagePreviewVisible ? styles.preview : styles.previewHide)}>
+        <Box className={classNames(styles.stageAndTargetWrapper, styles[stageSize], isPreview && stagePreviewVisible ? styles.preview : null)}>
             <StageWrapper
                 isFullScreen={isFullScreen}
                 isRendererSupported={isRendererSupported}
