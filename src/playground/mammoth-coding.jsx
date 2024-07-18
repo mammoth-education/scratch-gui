990fd3a3a9bbaf21784e742ed3ada191acd63c00
Mac <mac@MacdeMBP.(none)>
2024-07-18 10:18:58 +0800

    fix: 修复IOS无法马上使用cordova-plugin-device插件

--- src/playground/mammoth-coding.jsx
+++ src/playground/mammoth-coding.jsx
@@ -12,32 +12,36 @@ const onClickLogo = () => {
 
 const fixAndroidNotch = () => {
     console.log("fixAndroidNotch");
-    console.log("window.AndroidNotch",window.AndroidNotch);
+    console.log("window.AndroidNotch", window.AndroidNotch);
     if (window.AndroidNotch) {
         const style = document.documentElement.style;
- 
+
         // Apply insets as css variables
         window.AndroidNotch.getInsetTop(px => {
             style.setProperty("--notch-inset-top", px + "px");
         }, (err) => console.error("Failed to get insets top:", err));
-        
+
         window.AndroidNotch.getInsetRight(px => {
             style.setProperty("--notch-inset-right", px + "px");
         }, (err) => console.error("Failed to get insets right:", err));
-        
+
         window.AndroidNotch.getInsetBottom(px => {
             style.setProperty("--notch-inset-bottom", px + "px");
         }, (err) => console.error("Failed to get insets bottom:", err));
-        
+
         window.AndroidNotch.getInsetLeft(px => {
             style.setProperty("--notch-inset-left", px + "px");
         }, (err) => console.error("Failed to get insets left:", err));
     }
     // HUAWEI 设备能用 env ，所有防止重复设置 body 的 width 和 padding 
-    if(window.cordova && device.manufacturer == "HUAWEI"){
-        document.body.style.width = 'calc(100% - env(safe-area-inset-left) )';
-        document.body.style.padding  = '0';
-    }
+    // 添加 setTimeout 的原因是IOS加载需要时间,无法马上使用device
+    setTimeout(()=>{
+        if (window.cordova && device && device.manufacturer == "HUAWEI") {
+            document.body.style.width = 'calc(100% - env(safe-area-inset-left) )';
+            document.body.style.padding = '0';
+        }
+    },200);
+    
 };
 
 export default appTarget => {
@@ -53,27 +57,27 @@ export default appTarget => {
         // Warn before navigating away
         window.onbeforeunload = () => true;
     }
-    window.addEventListener("load",()=>{
-        if(window.cordova){
+    window.addEventListener("load", () => {
+        if (window.cordova) {
             // setTimeout(()=>{
-                
-                // document.body.style.margin = 'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)';
-                // document.body.style.width = 'calc(100% - env(safe-area-inset-left) )';
-                // document.body.style.width = 'calc(100% + env(safe-area-inset-left) )';
-                // document.body.style.height = 'calc(100% + env(safe-area-inset-bottom))';
-                // document.getElementsByTagName('html')[0].style.backgroundColor = 'hsla(177,100%,32%,1)';
-                // 固定全屏方向
-                // screen.orientation.lock('portrait');
-                screen.orientation.lock('landscape-primary');
+
+            // document.body.style.margin = 'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)';
+            // document.body.style.width = 'calc(100% - env(safe-area-inset-left) )';
+            // document.body.style.width = 'calc(100% + env(safe-area-inset-left) )';
+            // document.body.style.height = 'calc(100% + env(safe-area-inset-bottom))';
+            // document.getElementsByTagName('html')[0].style.backgroundColor = 'hsla(177,100%,32%,1)';
+            // 固定全屏方向
+            // screen.orientation.lock('portrait');
+            screen.orientation.lock('landscape-primary');
             // },100)
         }
     })
     // if(window.cordova && window.cordova.platformId == "android"){
     //     screen.orientation.lock('landscape-primary');
     // }
-    document.addEventListener("visibilitychange", ()=>{
+    document.addEventListener("visibilitychange", () => {
         if (document.visibilityState === "hidden") {
-            if(window.cordova && window.cordova.platformId == "ios"){
+            if (window.cordova && window.cordova.platformId == "ios") {
                 console.log("应用被切换到后台")
                 // 应用被切换到后台，执行相应的操作
                 document.body.style.width = 'calc(100% - env(safe-area-inset-left) )';
