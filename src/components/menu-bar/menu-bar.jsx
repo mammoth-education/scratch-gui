6b00f94008b0ac40d1fc64ae1c6f60b045a8478b
zl <zl@qq.com>
2026-03-27 15:39:30 +0800

    feat: 版本号改为1.1.0

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -556,10 +556,10 @@ class MenuBar extends React.Component {
                         defaultMessage="version"
                         description="Used to display the version number"
                         id="gui.about.version"
-                    />1.0.14
-                    <span>2026-03-13</span>
+                    />1.1.0
+                    <span>2026-03-26</span>
                 </div>
-                {this.state.updateShow ? <div className={styles.checkBox} >{this.state.loading ? loading : (this.state.updateTips ? latestVersion : updateTips)}</div> : null}
+                {/* {this.state.updateShow ? <div className={styles.checkBox} >{this.state.loading ? loading : (this.state.updateTips ? latestVersion : updateTips)}</div> : null} */}
                 <div><span className={styles.copyright}>2026@SunFounder</span></div>
             </>
         );
